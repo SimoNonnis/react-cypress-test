b6542eb14edcf7f9471949369deb42e61a31a429
Simon Nonnis <simon.nonnis@protonmail.com>
2018-08-03 17:59:54 +0100

    :recycle: Create text variable

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -3,4 +3,13 @@ describe('Test Form Input', () => {
     cy.visit('/');
     cy.focused().should('have.class', 'new-todo');
   });
+
+  it.only('should accept input value', () => {
+    const typedInput = 'Buy Pizza';
+
+    cy.visit('/');
+    cy.get('.new-todo')
+      .type(typedInput)
+      .should('have.value', typedInput);
+  });
 });
