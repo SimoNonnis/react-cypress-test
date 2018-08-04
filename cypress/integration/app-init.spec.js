88bd8b7bd33c8165e5601a55ac2f5b76cafaa6f4
Simon Nonnis <simon.nonnis@protonmail.com>
2018-08-04 12:06:30 +0100

    :recycle: Use alias and wait

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,8 +1,9 @@
 describe('Test App init', () => {
   it('should load list of todos from API', () => {
     cy.server();
-    cy.route('GET', '/api/todos', 'fixture:todos');
+    cy.route('GET', '/api/todos', 'fixture:todos').as('load');
     cy.visit('/');
+    cy.wait('@load');
     cy.get('.todo-list li').should('have.length', 4);
   });
 });
