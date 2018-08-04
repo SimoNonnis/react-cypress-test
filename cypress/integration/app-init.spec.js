cd2621f7bb164dfdf090a19b75cd3b01f12b1a2b
Simon Nonnis <simon.nonnis@protonmail.com>
2018-08-04 10:02:11 +0100

    :sparkles: Stub GET call to /api/todos

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,6 +1,10 @@
 describe('Test App init', () => {
   it('should load list of todos from API', () => {
+    cy.server();
+    cy.route('GET', '/api/todos', [
+      { id: 1, name: 'Make pizza', completed: false }
+    ]);
     cy.visit('/');
-    cy.get('.todo-list li').should('have.length', 3);
+    cy.get('.todo-list li').should('have.length', 1);
   });
 });
