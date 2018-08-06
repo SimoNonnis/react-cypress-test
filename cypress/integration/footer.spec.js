e68103f3b0b175d993e78dd4bae6b2a58f41639e
Simon Nonnis <simon.nonnis@protonmail.com>
2018-08-06 08:57:48 +0100

    :recycle: Refactor using cy.wrap() and array of options

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,9 +1,16 @@
 describe('Test Footer', () => {
   it('should filter todos', () => {
+    const filters = [
+      { link: 'All', expectedLength: 4 },
+      { link: 'Active', expectedLength: 2 },
+      { link: 'Completed', expectedLength: 2 }
+    ];
+
     cy.loadAndVisit('fixture:mixed_todos');
 
-    cy.contains('Active').click();
-
-    cy.get('.todo-list li').should('have.length', 2);
+    cy.wrap(filters).each(filter => {
+      cy.contains(filter.link).click();
+      cy.get('.todo-list li').should('have.length', filter.expectedLength);
+    });
   });
 });
